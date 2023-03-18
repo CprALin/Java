6a8702e246383674abfb871fef7de5b48258823c
CprAlin <stfalin@yahoo.com>
2023-03-18 17:11:48 +0200

    here is delete window

--- deletePhoneWindow.java
+++ deletePhoneWindow.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class deletePhoneWindow extends JFrame{
+    /* here is delete window */
       private JButton delete;
       private JTextField name;
       private EventDP e;
