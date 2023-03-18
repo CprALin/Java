f95c516f6979c4b4139f8d94a1ed5508ec4ffc06
CprAlin <stfalin@yahoo.com>
2023-03-18 17:11:01 +0200

    Main Window

--- mainWindow.java
+++ mainWindow.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class mainWindow extends JFrame{
+    /*main window */
     private EventMW e;
     private JButton[] b;
     
